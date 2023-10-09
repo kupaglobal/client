a39cbf52f08125c0e5ffa3665d5e9f3bb9c86762
Bakani Pilime <bakani@baobabcircle.com>
2023-10-09 05:33:53 +0200

    Fix eslint issue

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import WelcomePopup from '../features/Dashboard/Popup/WelcomePopup'
 import { authStore } from '../store/auth'
 
