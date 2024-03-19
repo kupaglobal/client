998b7119090e0f8f55a3080513ef3298fb8065f2
Bakani Pilime <bzmpilime@gmail.com>
2024-03-19 23:39:09 +0200

    Tag colleagues to reminder

--- src/features/Dashboard/Reminders/ReminderList.js
+++ src/features/Dashboard/Reminders/ReminderList.js
@@ -1,35 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Button } from 'primereact/button';
 import { Avatar } from 'primereact/avatar';
 import { InputText } from 'primereact/inputtext';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { ListBox } from 'primereact/listbox';
+import { OverlayPanel } from 'primereact/overlaypanel';
 import { loadingSkeleton } from '../../../utils';
 import { DashboardService } from '../../../services/dashboard.service';
+import OrganisationService from '../../../services/organisation.service';
 
 const RemindersList = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
-  const [taggedColleague, setTaggedColleague] = useState('');
-  const [taggedColleagues, setTaggedColleagues] = useState([]);
 
   const [isLoading, setIsLoading] = useState(false);
+  const [tagUserIsLoading, setTagUserIsLoading] = useState(false);
   const [selectedTodo, setSelectedTodo] = useState(null)
 
+  const [showColleaguesOverlay, setShowColleaguesOverlay] = useState(false)
+  const [colleaguesOverlayPosition, setColleaguesOverlayPosition] = useState(null)
+
+  const [colleagues, setColleagues] = useState([])
+
+  const overlayPanel = useRef(null);
   const addTodo = async () => {
     setIsLoading(true)
     if (newTodo.trim() !== '') {
       try {
         const newTodoDto = {
           body: newTodo,
-          completed: false,
-          tags: taggedColleagues.map(colleague => colleague.id)
+          completed: false
         }
-        await DashboardService.addReminder(newTodoDto)
+        setTodos([ newTodoDto, ...todos ])
         setNewTodo('');
+
+        await DashboardService.addReminder(newTodoDto)
         setShouldRefetch(true)
         setIsLoading(false)
       } catch (e) {
+        setShouldRefetch(true)
         setIsLoading(false)
       }
     }
@@ -39,11 +48,13 @@ const RemindersList = () => {
     try {
       setSelectedTodo(todoId)
       setIsLoading(true)
+      setTodos(todos.filter(todo => todo.id!==todoId))
       await DashboardService.deleteReminder(todoId)
       setShouldRefetch(true)
       setIsLoading(false)
       setSelectedTodo('')
     } catch (e) {
+      setShouldRefetch(true)
       setIsLoading(false)
     }   
     // const updatedTodos = todos.filter((_, i) => i !== index);
@@ -61,6 +72,7 @@ const RemindersList = () => {
       // Convert the color to RGBA format with varying opacity
       const color = `rgba(93, 23, 235, ${opacity})`;
 
+      user.initials = `${user.firstName[0]}${user.lastName[0]}`
       return (
         <div
           key={index}
@@ -77,6 +89,7 @@ const RemindersList = () => {
   };
  
   const [shouldRefetch, setShouldRefetch] = useState(true)
+  const [shouldRefetchColleagues, setShouldRefetchColleagues] = useState(true)
   useEffect(() => {
     async function getReminders() {
       try {
@@ -92,7 +105,20 @@ const RemindersList = () => {
     if (shouldRefetch) {
       getReminders();
     }
-  }, [shouldRefetch])
+
+    async function getColleagues() {
+      try {
+        const {data: { members }} = await OrganisationService.getOrganisationMembers();
+        setColleagues(members)
+        setShouldRefetchColleagues(false)
+      } catch(e) {
+        console.error(e)
+      }
+    }
+    if (shouldRefetchColleagues) {
+      getColleagues();
+    }
+  }, [shouldRefetch, shouldRefetchColleagues])
 
   const [editableIndex, setEditableIndex] = useState(null);
 
@@ -100,13 +126,30 @@ const RemindersList = () => {
     setEditableIndex(index);
   };
 
-  const handleEnter = async (event, index) => {
-    if (event.key === 'Enter') {
+  const handleEnter = async (event, index, selectedTodoId) => {
+    if (selectedTodoId) {
+      setSelectedTodo(selectedTodoId)
+    }    
+    if (event.key === 'Enter' && !showColleaguesOverlay) {
       try {
-        updateReminder(todos[index], index)
+        if (newTodo) {
+          addTodo()
+        } else {
+          updateReminder(todos[index], index)
+        }
       } catch (e) {
         console.error(e)
       }
+    } else if (event.key === '@') {
+      const inputElement = event.target;
+      const rect = inputElement.getBoundingClientRect();
+
+      setColleaguesOverlayPosition({ left: rect.left, top: rect.top + rect.height });
+      setShouldRefetchColleagues(true) 
+
+      overlayPanel.current.toggle(event)
+    } else {
+      overlayPanel.current.to = null
     }
   }
 
@@ -160,15 +203,25 @@ const RemindersList = () => {
                 </div>
               )}
             </div>
-            <div className="flex flex-end">
-              <Button
+            <div className="flex flex-end text-right">
+            <Button
                 loading={isLoading && selectedTodo === todo.id}
                 size='small'
                 icon="pi pi-trash"
                 className="p-button-danger text-right border-none"
-                style={{ border: 'none'}}
+                style={{ border: 'none', paddingRight: '0px'}}
                 text severity="danger"
                 onClick={() => removeTodo(todo.id)}
+              />
+              <Button
+                loading={tagUserIsLoading}
+                size='small'
+                icon="pi pi-user-plus"
+                className="text-right border-none"
+                style={{ border: 'none', paddingLeft: '0px'}}
+                text
+                disabled={availableColleagues(colleagues, todo.id).length === 0}
+                onClick={(e) => handleEnter({...e, key: '@'}, null, todo.id)}
               />
             </div>
             </div>
@@ -178,12 +231,42 @@ const RemindersList = () => {
     );
   }
 
+  async function addColleagueToReminder(colleague, todoId) {
+    try {
+      setTagUserIsLoading(true)
+      const todo = todos.filter(todo => todo.id === todoId)[0]
+      
+      const currentTags = todo && todo && todo.taggedUsers ? todo?.taggedUsers.map(taggedUser => taggedUser.id) : []
+      if (!currentTags.includes(todo.id)) {
+        await DashboardService.updateReminder(selectedTodo, {
+          tags: [...currentTags, colleague.id]
+        })
+  
+        setTagUserIsLoading(false)
+        setShouldRefetch(true)
+        overlayPanel.current.toggle(null)
+      }
+    } catch (e) {
+      overlayPanel.current.toggle({})
+      console.error(`Exception when adding ${colleague.id} to toDo ${todoId}, e: ${e}`)
+    }
+  }
+
+  function availableColleagues(colleagues, selectedTodoId) {
+    if (!selectedTodoId) return colleagues
+    const todo = todos.filter(todo => todo && todo.id === selectedTodoId)[0]
+    const todoTags = todo?.taggedUsers.map(taggedUser => taggedUser.id) ?? []
+
+    return colleagues.filter(colleague => todoTags.indexOf(colleague.id) === -1)
+  }
+
   return (
     <div className='w-full w-20rem' >
       <h3>Reminders</h3>
       <div className="p-inputgroup text-sm">
         <InputText
-        className='text-sm'
+          onKeyUp={(event) => handleEnter(event, null)}
+          className='text-sm'
           value={newTodo}
           onChange={(e) => setNewTodo(e.target.value)}
           placeholder="Add new reminder"
@@ -202,6 +285,33 @@ const RemindersList = () => {
         // itemTemplate={(todo, index) => (
         // )}
       />}
+      <OverlayPanel
+        dismissable={true}
+        ref={overlayPanel}
+        onHide={() => setShowColleaguesOverlay(false)}
+        position={colleaguesOverlayPosition}
+      >
+        {/* Your list of colleagues goes here */}
+        <ul className='p-list-unstyled w-15rem'>
+          {availableColleagues(colleagues, selectedTodo).map((colleague, index) => (
+            <li key={index} className="my-2 p-cursor-pointer colleague-item w-full" onClick={() => addColleagueToReminder(colleague, selectedTodo)}>
+              <div className="gap-2 flex flex-row p-align-center">
+                <Avatar
+                  className="p-mr-2"
+                  label={colleague.firstName.charAt(0)}
+                  shape="circle"
+                  size="small"
+                />
+                <div>
+                  <span>{colleague.firstName}</span>
+                  <br />
+                  <span className="p-text-secondary" style={{fontSize: '8pt'}}>{colleague.role}</span>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </OverlayPanel>
     </div>
   );
 };
