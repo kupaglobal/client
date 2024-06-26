3546860bda1909e57448cd954ace686a9d9e774c
Bakani Pilime <bzmpilime@gmail.com>
2024-06-26 14:33:05 +0200

    Take all new users to dashboard?welcome

--- src/pages/Auth/UpsertProfile.js
+++ src/pages/Auth/UpsertProfile.js
@@ -29,7 +29,7 @@ const UpsertProfile = () => {
             if (authResponse && authResponse.emailVerified === true) { // successful
                 const {data: profileRes} = await AuthService.getProfile()
                 dispatch({ type: SET_LOGGED_IN_USER, payload: profileRes })
-                if (IsNewUser && !authResponse.organisationId) {
+                if (IsNewUser) {
                     goTo('/dashboard?welcome')
                 } else {
                     goTo('/dashboard')
