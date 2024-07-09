8d1d8a1d43f28068ce7393ae80286a9d8e7e48b1
Bakani Pilime <bzmpilime@gmail.com>
2024-07-09 09:34:16 +0200

    Fix .organisation is null bug

--- src/features/Dashboard/Popup/WelcomePopup.js
+++ src/features/Dashboard/Popup/WelcomePopup.js
@@ -78,7 +78,7 @@ export default function WelcomePopup({ user }) {
       </div>
     </Dialog>
     <Dialog
-      header={`Joined ${user.organisation.name}`}
+      header={`Joined ${user.organisation?.name}`}
       visible={isNewFacilitator}
       style={{ width: "30vw" }}
       breakpoints={{ "960px": "75vw", "641px": "100vw" }}
